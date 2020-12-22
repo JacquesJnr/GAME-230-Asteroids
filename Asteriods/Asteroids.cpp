94e9707e155eb29784d9622ad2332c4df2e7f98c
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-12-22 12:05:00 +0400

    Renamed Entity Class to GameObject and added Bullet Class

--- Asteriods/Asteroids.cpp
+++ Asteriods/Asteroids.cpp
@@ -6,14 +6,14 @@
 using namespace sf;
 
 
-class Entity
+class GameObject
 {
    public:
    float xPos,yPos,deltaX,deltaY,radius,angle;
    bool life;
    std::string name;
 
-   Entity()
+   GameObject()
    {
      life = 1;
    }
@@ -36,10 +36,10 @@ class Entity
      app.draw(circle);
    }
 
-   virtual ~Entity() {};
+   virtual ~GameObject() {};
 };
 
-class Asteroid : public Entity
+class Asteroid : public GameObject
 {
 public:
     Asteroid()
@@ -59,7 +59,7 @@ public:
     }
 };
 
-class Player : public Entity
+class Player : public GameObject
 {
 public:
     bool thrust;
@@ -97,6 +97,35 @@ public:
         if (yPos > HEIGHT) yPos = 0; if (yPos < 0) yPos = HEIGHT;
     }
 
+}; 
+
+bool isCollide(GameObject* a, GameObject* b)
+{
+    return (b->xPos - a->xPos) * (b->xPos - a->xPos) +
+        (b->yPos - a->yPos) * (b->yPos - a->yPos) <
+        (a->radius + b->radius) * (a->radius + b->radius);
+}
+
+
+class Bullet : public GameObject
+{
+public:
+    Bullet()
+    {
+        name = "bullet";
+    }
+
+    void  update()
+    {
+        deltaX = cos(angle * degsToRad) * 6;
+        deltaY = sin(angle * degsToRad) * 6;
+        // angle+=rand()%7-3;  /*try this*/
+        xPos += deltaX;
+        yPos += deltaY;
+
+        if (xPos > WIDTH || xPos<0 || yPos>HEIGHT || yPos < 0) life = 0;
+    }
+
 };
 
 int main() {
@@ -106,6 +135,19 @@ int main() {
     RenderWindow window(VideoMode(WIDTH, HEIGHT), "SFML Asteroids!");
     window.setFramerateLimit(60);
 
+    std::list<GameObject*> entities;
+
+    for (int i = 0; i < 15; i++)
+    {
+        Asteroid* a = new Asteroid();
+        a->settings(rand() % WIDTH, rand() % HEIGHT, rand() % 360, 25);
+    }
+
+    Player* p = new Player();
+    p->settings(200, 200, 0, 20);
+    entities.push_back(p);
+
+
     while (window.isOpen())
     {
         Event event;
@@ -113,10 +155,86 @@ int main() {
         {
             if (event.type == Event::Closed)
                 window.close();
+
+            if (event.type == Event::KeyPressed)
+                if (event.key.code == Keyboard::Space)
+                {
+                    Bullet* b = new Bullet();
+                    b->settings(p->xPos, p->yPos, p->angle, 10);
+                    entities.push_back(b);
+                }
         }
 
-        window.clear();
+        if (Keyboard::isKeyPressed(Keyboard::Right)) p->angle += 3;
+        if (Keyboard::isKeyPressed(Keyboard::Left))  p->angle -= 3;
+        if (Keyboard::isKeyPressed(Keyboard::Up)) p->thrust = true;
+        else p->thrust = false;
+
+
+        for (auto a : entities)
+            for (auto b : entities)
+            {
+                if (a->name == "asteroid" && b->name == "bullet")
+                    if (isCollide(a, b))
+                    {
+                        a->life = false;
+                        b->life = false;
+
+                        GameObject* e = new GameObject();
+                        e->settings(a->xPos, a->yPos);
+                        e->name = "explosion";
+                        entities.push_back(e);
+
+
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (a->radius == 15) continue;
+                            GameObject* e = new Asteroid();
+                            e->settings(a->xPos, a->yPos, rand() % 360, 15);
+                            entities.push_back(e);
+                        }
+
+                    }
+
+                if (a->name == "player" && b->name == "asteroid")
+                    if (isCollide(a, b))
+                    {
+                        b->life = false;
+
+                        GameObject* e = new GameObject();
+                        e->settings(sExplosion_ship, a->xPos, a->yPos);
+                        e->name = "explosion";
+                        entities.push_back(e);
+
+                        p->settings( WIDTH / 2, HEIGHT / 2, 0, 20);
+                        p->deltaX = 0; p->deltaY = 0;
+                    }
+            }
+
+        for (auto e : entities)
+            if (e->name == "explosion")
+                e->life = 0;
+
+        if (rand() % 150 == 0)
+        {
+            Asteroid* a = new Asteroid();
+            a->settings(0, rand() % WIDTH, rand() % 360, 25);
+            entities.push_back(a);
+        }
+
+        for (auto i = entities.begin(); i != entities.end();)
+        {
+            GameObject* e = *i;
+
+            e->update();
+
+            if (e->life == false) { i = entities.erase(i); delete e; }
+            else i++;
+        }
+
+        //////draw//////
+        window.draw(background);
+        for (auto i : entities) i->draw(window);
         window.display();
     }
-
 }
